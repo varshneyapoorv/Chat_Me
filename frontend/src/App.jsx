4e49ff59c41be86a6cd5d76a9494373d13b2aebf
Apoorv Varshney <apoorvvarshney8@gmail.com>
2024-12-11 16:56:48 +0530

    5.37.38

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import {createBrowserRouter, Router, RouterProvider} from 'react-router-dom'
 import HomePage from './components/HomePage'
 import Signup from './components/Signup'
 import Login from './components/Login'
+import { useSelector } from 'react-redux';
+import io from 'socket.io-client';
 
 const router = createBrowserRouter([
   {
@@ -19,6 +21,22 @@ const router = createBrowserRouter([
     element : <Login />
   }
 ])
+
+function app (){
+
+  const [socket, setsocket] = useState(null);
+  const {authUser} = useSelector(store=>store.user);
+  useEffect(()=>{
+    if(authUser){
+      const socket = io('http://localhost:1234',{
+        query:{
+          userId:authUser._id
+        }
+      });
+      setsocket(socket);
+    }
+  }, [authUser])
+}
 
 
 const App = () => {
